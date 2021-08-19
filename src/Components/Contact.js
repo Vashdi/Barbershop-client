9b74a944e83643b06adce30daf63046091bb8618
Nir Vashdi <vashdi7002@gmail.com>
2021-08-19 23:27:02 +0300

    added waze

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React from 'react'
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import './contact.css'
 
 const Contact = (props) => {
 
@@ -10,7 +11,9 @@ const Contact = (props) => {
             Vashdi7002@gmail.com :אימייל <br /><br /><br />
             <LocationOnIcon fontSize="large" color="secondary" /> <br />
             כתובת: לחי 2, קרית ים <br />
-
+            <a className="waze" href={`https://waze.com/ul?ll=32.8539957,35.0709409navigate=yes`}>
+                Waze נווט באמצעות
+            </a>
         </section >
     )
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import { Icon } from '@iconify/react';
 
 const Footer = (props) => {
     return (
@@ -12,7 +13,14 @@ const Footer = (props) => {
                 <li className="social-links" key="facebook"><a href="http://www.facebook.com/snir.mishaelof"><i className="fa fa-facebook"></i></a></li>
                 <li className="social-links" key="instagram"><a href="http://www.instagram.com/snirmishaelof_barbershop/"><i className="fa fa-instagram"></i></a></li>
                 <li><a href="https://wa.me/+972526662146"><WhatsAppIcon fontSize='large' /></a></li>
+                <li>
+                    <a style={{ position: 'absolute', bottom: '43%', left: '56%' }}
+                        href={`https://waze.com/ul?ll=32.8539957,35.0709409navigate=yes`}>
+                        <Icon style={{ top: 40 }} icon="mdi:waze" width="35" height="35" />
+                    </a>
+                </li>
             </ul>
+
             <nav id="nav-wrap2">
                 <ul id="nav2" className="nav2">
                     <li><a className="smoothscroll" href="#home">בית</a></li>
