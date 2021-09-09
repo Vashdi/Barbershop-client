664d2f4af179f28f2fe8c062a5019a2eccc2335b
Vashdi <62853009+Vashdi@users.noreply.github.com>
2021-09-09 18:56:33 +0300

    price change

--- src/Components/Price.js
+++ src/Components/Price.js
@@ -1,18 +1,41 @@
 import React from "react";
 import './Price.css'
 import 'aos/dist/aos.css';
-
 const Price = () => {
 
     return (<section data-aos="fade-up" id="price">
         <h1 data-aos="zoom-out-up" className="priceTitle">מחירון&nbsp;מספרה</h1>
-        <div className="priceContainer">
+        <div className="pricesContainer">
+            <div data-aos="zoom-out-up" className="pricePicture">
+                <img src={'images/pricepic.jpg'} alt="" />
+            </div>
+            <div data-aos="zoom-out-up" className="pricesBoard">
+                <h1 style={{ color: 'white' }}>המחירים שלנו</h1>
+                <ul>
+                    <li>תספורת גבר........................................................₪50</li>
+                    <li>₪תספורת וזקן................................................60</li>
+                    <li>₪צבע..............................................................100</li>
+                    <li>₪סידור זקן.......................................................20</li>
+                    <li>תספורת גבר........................................................₪50</li>
+                    <li>₪תספורת וזקן................................................60</li>
+                    <li>₪צבע..............................................................100</li>
+                    <li>₪סידור זקן.......................................................20</li>
+                    <li>תספורת גבר........................................................₪50</li>
+                    <li>₪תספורת וזקן................................................60</li>
+                    <li>₪צבע..............................................................100</li>
+                    <li>₪סידור זקן.......................................................20</li>
+                    <li>תספורת גבר........................................................₪50</li>
+                    <li>₪תספורת וזקן................................................60</li>
+                    <li>₪צבע..............................................................100</li>
+                    <li>₪סידור זקן.......................................................20</li>
+                </ul>
+            </div>
+        </div>
+        {/* <div className="priceContainer">
             <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6" className="pricingCard">
                 <div data-aos="zoom-out-up" class="pricing-plan">
                     <div class="pricing-header">
-                        <h4 class="pricing-title">~~~~</h4>
                         <div class="pricing-cost">גבר</div>
-                        <div class="pricing-save">~~~~</div>
                     </div>
                     <ul class="pricing-features">
                         <li>תספורת........................................................₪50</li>
@@ -25,9 +48,7 @@ const Price = () => {
             <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6" className="pricingCard">
                 <div data-aos="zoom-out-up" class="pricing-plan">
                     <div class="pricing-header red">
-                        <h4 class="pricing-title">~~~~</h4>
                         <div class="pricing-cost">ילד</div>
-                        <div class="pricing-save">~~~~</div>
                     </div>
                     <ul class="pricing-features">
                         <li>תספורת........................................................₪50</li>
@@ -40,9 +61,7 @@ const Price = () => {
             <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12" className="pricingCard">
                 <div data-aos="zoom-out-up" class="pricing-plan">
                     <div class="pricing-header secondary">
-                        <h4 class="pricing-title">~~~~</h4>
                         <div class="pricing-cost">צבע</div>
-                        <div class="pricing-save">~~~~</div>
                     </div>
                     <ul class="pricing-features">
                         <li>תספורת........................................................₪50</li>
@@ -52,7 +71,7 @@ const Price = () => {
                     </ul>
                 </div>
             </div>
-        </div>
+        </div> */}
     </section >)
 }
 
