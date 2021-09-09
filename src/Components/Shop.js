2e79870e228604165fb8512dd61d9205bd59be30
Vashdi <62853009+Vashdi@users.noreply.github.com>
2021-09-09 16:39:42 +0300

    css

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -9,7 +9,6 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import './Shop.css'
-import backPic from './Pictuers/shop.jpeg'
 
 const useStyles = makeStyles({
     root: {
