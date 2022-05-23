b8df6a7638c88117fad259297ed66837a2aac9a1
Vashdi <Vashdi7002@gmail.com>
2022-05-23 15:14:16 +0300

    popup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,104 @@
-import React from 'react'
-import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Gallery from './Components/Gallery';
-import Contact from './Components/Contact';
-import Price from './Components/Price';
-import { useEffect, useState } from 'react'
-import Shop from './Components/Shop';
-import Appointment from './Components/Appointment';
-import { useSelector } from 'react-redux';
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Gallery from "./Components/Gallery";
+import Contact from "./Components/Contact";
+import Price from "./Components/Price";
+import { useEffect, useState } from "react";
+import Shop from "./Components/Shop";
+import Appointment from "./Components/Appointment";
+import { useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
-    const storeData = useSelector(state => state);
-    const [logged, setLogged] = useState(false);
+  const storeData = useSelector((state) => state);
+  const [logged, setLogged] = useState(false);
+  const classes = useStyles();
+  const [openModal, setOpenModal] = useState(false);
 
-    useEffect(() => {
-        setLogged(storeData.AuthReducer.user ? true : false);
-    }, [storeData.AuthReducer.user])
+  useEffect(() => {
+    if (storeData.AuthReducer.user) {
+      const pop_status = localStorage.getItem("modal_status");
+      if (!pop_status) {
+        setOpenModal(true);
+      }
+    } else {
+      localStorage.removeItem("modal_status");
+    }
+    setLogged(storeData.AuthReducer.user ? true : false);
+  }, [storeData.AuthReducer.user]);
 
-    return (
-        <div className="App">
-            <Header />
-            <About />
-            {logged ? <Appointment /> : null}
-            <Gallery />
-            <Price />
-            <Shop />
-            <Contact />
-            <Footer />
-        </div>
-    );
+  const closeModal = () => {
+    localStorage.setItem("modal_status", 1);
+    setOpenModal(false);
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      {logged ? <Appointment /> : null}
+      <Gallery />
+      <Price />
+      <Shop />
+      <Contact />
+      <Footer />
+      <div>
+        <button type="button">react-transition-group</button>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={openModal}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={openModal}>
+            <div className={classes.paper}>
+              <h2 id="transition-modal-title" style={{ textAlign: "center" }}>
+                התראה לגבי אי הגעה לתורים
+              </h2>
+              <p
+                id="transition-modal-description"
+                style={{ textAlign: "center" }}
+              >
+                ,לקוחות יקרים <br />
+                לקוח אשר לא יגיע לתור שקבע ללא ביטולו יחוייב בתשלום התספורת
+              </p>
+              <div
+                style={{ textAlign: "center", cursor: "pointer" }}
+                onClick={closeModal}
+              >
+                סגור
+              </div>
+            </div>
+          </Fade>
+        </Modal>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,166 +1,225 @@
-import React, { useState } from 'react'
-import SignupService from '../services/register'
-import FormControl from '@material-ui/core/FormControl';
-import { Button, IconButton, InputAdornment, InputLabel, OutlinedInput, ThemeProvider } from '@material-ui/core';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import { createTheme } from '@material-ui/core/styles';
-import { deepOrange } from '@material-ui/core/colors';
-import { useForm } from 'react-hook-form';
-import Swal from 'sweetalert2'
-import { useDispatch } from 'react-redux';
-import appService from '../services/appointment'
-import { Link, useHistory } from 'react-router-dom';
-import Notify from '../services/Notify';
-
+import React from "react";
+import SignupService from "../services/register";
+import FormControl from "@material-ui/core/FormControl";
+import {
+  Button,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  ThemeProvider,
+} from "@material-ui/core";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import { createTheme } from "@material-ui/core/styles";
+import { deepOrange } from "@material-ui/core/colors";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { useDispatch } from "react-redux";
+import appService from "../services/appointment";
+import { Link, useHistory } from "react-router-dom";
+import Notify from "../services/Notify";
 
 const theme = createTheme({
-    palette: {
-        primary: deepOrange,
-    },
+  palette: {
+    primary: deepOrange,
+  },
 });
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: '25ch',
-            display: 'flex',
-            flexWrap: 'wrap',
-        }
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+      display: "flex",
+      flexWrap: "wrap",
     },
-    margin: {
-        margin: theme.spacing(1),
-    },
-    withoutLabel: {
-        marginTop: theme.spacing(3),
-    },
-    textField: {
-        width: '25ch',
-    },
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+  textField: {
+    width: "25ch",
+  },
 }));
 
 const Toast = Swal.mixin({
-    toast: true,
-    position: 'bottom-left',
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-        toast.addEventListener('mouseenter', Swal.stopTimer)
-        toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-})
+  toast: true,
+  position: "bottom-left",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener("mouseenter", Swal.stopTimer);
+    toast.addEventListener("mouseleave", Swal.resumeTimer);
+  },
+});
 
 const Signup = (props) => {
-    let history = useHistory();
-    const dispatch = useDispatch();
-    const classes = useStyles();
-    const { register, handleSubmit, formState: { errors } } = useForm();
+  let history = useHistory();
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    const send = async (data) => {
-        try {
-            console.log(data);
-            const user = await SignupService.register({
-                firstname: data.firstName,
-                lastname: data.lastName,
-                phone: data.phone,
-            });
-            dispatch({ type: "LOGIN", payload: user });
-            appService.setToken(user.token)
-            Toast.fire({
-                icon: 'success',
-                title: '!ההרשמה בוצעה בהצלחה'
-            })
-            history.push('/');
-        } catch (error) {
-            Notify.errorHandler(error.message);
-        }
+  const send = async (data) => {
+    try {
+      console.log(data);
+      const user = await SignupService.register({
+        firstname: data.firstName,
+        lastname: data.lastName,
+        phone: data.phone,
+      });
+      dispatch({ type: "LOGIN", payload: user });
+      appService.setToken(user.token);
+      Toast.fire({
+        icon: "success",
+        title: "!ההרשמה בוצעה בהצלחה",
+      });
+      history.push("/");
+    } catch (error) {
+      Notify.errorHandler(error.message);
     }
+  };
 
-    return (<section id="signup">
-        <div className={classes.root}>
-            <form onSubmit={handleSubmit((data => send(data)))}>
-                <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
-                    <InputLabel dir="rtl" style={{ fontSize: 'medium', left: 25 }} htmlFor="outlined-adornment-password">מספר פלאפון</InputLabel>
-                    <OutlinedInput style={{ width: '200%', fontSize: 'large', left: 25 }}
-                        id="outlined-adornment-password"
-                        type='text'
-                        endAdornment={
-                            <InputAdornment position="end">
-                            </InputAdornment>
-                        }
-                        labelWidth={80}
-                        {...register("phone", {
-                            required: { value: true, message: "הכנס מספר פלאפון" },
-                            maxLength: { value: 10, message: "מספר הפלאפון ארוך מדי" },
-                            minLength: { value: 10, message: "מספר הפלאפון קצר מדי" },
-                            pattern: { value: /^[0-9]*$/i, message: "מספר פלאפון יכול להכיל רק מספרים" }
-                        })}
-                    />
-                </FormControl>
-                {errors.phone && <span style={{ color: 'red' }}>{errors.phone.message}</span>}
-                <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
-                    <InputLabel style={{ fontSize: 'medium', left: 25 }} htmlFor="outlined-adornment-password">שם פרטי</InputLabel>
-                    <OutlinedInput style={{ width: '200%', fontSize: 'large', left: 25 }}
-                        id="outlined-adornment-password"
-                        type='text'
-                        dir="rtl"
-                        endAdornment={
-                            <InputAdornment position="end">
-                            </InputAdornment>
-                        }
-                        labelWidth={60}
-                        {...register("firstName", {
-                            required: { value: true, message: "הכנס שם פרטי" },
-                            maxLength: { value: 20, message: "שם ארוך מדי" },
-                            minLength: { value: 2, message: "שם קצר מדי" },
-                            pattern: { value: /^[a-z\u0590-\u05fe]+$/i, message: "שם פרטי לא יכול להכיל מספרים או תווים מיוחדים" }
-                        })}
-                    />
-                </FormControl>
-                {errors.firstName && <span style={{ color: 'red' }}>{errors.firstName.message}</span>}
-                <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
-                    <InputLabel style={{ fontSize: 'medium', left: 25 }} htmlFor="outlined-adornment-password">שם משפחה</InputLabel>
-                    <OutlinedInput style={{ width: '200%', fontSize: 'large', left: 25 }}
-                        id="outlined-adornment-password"
-                        type='text'
-                        dir="rtl"
-                        endAdornment={
-                            <InputAdornment position="end">
-                            </InputAdornment>
-                        }
-                        labelWidth={73}
-                        {...register("lastName", {
-                            required: { value: true, message: "הכנס שם משפחה" },
-                            maxLength: { value: 20, message: "שם ארוך מדי" },
-                            minLength: { value: 2, message: "שם קצר מדי" },
-                            pattern: { value: /^[a-z\u0590-\u05fe]+$/i, message: "שם משפחה לא יכול להכיל מספרים או תווים מיוחדים" }
-                        })}
-                    />
-                </FormControl>
-                {errors.lastName && <span style={{ color: 'red' }}>{errors.lastName.message}</span>}
+  return (
+    <section id="signup">
+      <div className={classes.root}>
+        <form onSubmit={handleSubmit((data) => send(data))}>
+          <FormControl
+            className={clsx(classes.margin, classes.textField)}
+            variant="outlined"
+          >
+            <InputLabel
+              dir="rtl"
+              style={{ fontSize: "medium", left: 25 }}
+              htmlFor="outlined-adornment-password"
+            >
+              מספר פלאפון
+            </InputLabel>
+            <OutlinedInput
+              style={{ width: "200%", fontSize: "large", left: 25 }}
+              id="outlined-adornment-password"
+              type="text"
+              endAdornment={<InputAdornment position="end"></InputAdornment>}
+              labelWidth={80}
+              {...register("phone", {
+                required: { value: true, message: "הכנס מספר פלאפון" },
+                maxLength: { value: 10, message: "מספר הפלאפון ארוך מדי" },
+                minLength: { value: 10, message: "מספר הפלאפון קצר מדי" },
+                pattern: {
+                  value: /^[0-9]*$/i,
+                  message: "מספר פלאפון יכול להכיל רק מספרים",
+                },
+              })}
+            />
+          </FormControl>
+          {errors.phone && (
+            <span style={{ color: "red" }}>{errors.phone.message}</span>
+          )}
+          <FormControl
+            className={clsx(classes.margin, classes.textField)}
+            variant="outlined"
+          >
+            <InputLabel
+              style={{ fontSize: "medium", left: 25 }}
+              htmlFor="outlined-adornment-password"
+            >
+              שם פרטי
+            </InputLabel>
+            <OutlinedInput
+              style={{ width: "200%", fontSize: "large", left: 25 }}
+              id="outlined-adornment-password"
+              type="text"
+              dir="rtl"
+              endAdornment={<InputAdornment position="end"></InputAdornment>}
+              labelWidth={60}
+              {...register("firstName", {
+                required: { value: true, message: "הכנס שם פרטי" },
+                maxLength: { value: 20, message: "שם ארוך מדי" },
+                minLength: { value: 2, message: "שם קצר מדי" },
+                pattern: {
+                  value: /^[a-z\u0590-\u05fe]+$/i,
+                  message: "שם פרטי לא יכול להכיל מספרים או תווים מיוחדים",
+                },
+              })}
+            />
+          </FormControl>
+          {errors.firstName && (
+            <span style={{ color: "red" }}>{errors.firstName.message}</span>
+          )}
+          <FormControl
+            className={clsx(classes.margin, classes.textField)}
+            variant="outlined"
+          >
+            <InputLabel
+              style={{ fontSize: "medium", left: 25 }}
+              htmlFor="outlined-adornment-password"
+            >
+              שם משפחה
+            </InputLabel>
+            <OutlinedInput
+              style={{ width: "200%", fontSize: "large", left: 25 }}
+              id="outlined-adornment-password"
+              type="text"
+              dir="rtl"
+              endAdornment={<InputAdornment position="end"></InputAdornment>}
+              labelWidth={73}
+              {...register("lastName", {
+                required: { value: true, message: "הכנס שם משפחה" },
+                maxLength: { value: 20, message: "שם ארוך מדי" },
+                minLength: { value: 2, message: "שם קצר מדי" },
+                pattern: {
+                  value: /^[a-z\u0590-\u05fe]+$/i,
+                  message: "שם משפחה לא יכול להכיל מספרים או תווים מיוחדים",
+                },
+              })}
+            />
+          </FormControl>
+          {errors.lastName && (
+            <span style={{ color: "red" }}>{errors.lastName.message}</span>
+          )}
 
-                <ThemeProvider theme={theme}>
-                    <Button style={{ width: '300%', left: 85 }} type="submit" variant="contained" color="primary" className={classes.margin}>
-                        <span style={{ fontSize: "large" }}>הרשם עכשיו</span>
-                    </Button>
-                </ThemeProvider>
-            </form>
-            <hr style={{ border: '1px solid grey', width: '98%' }} />
-            <Link style={{ display: "flex", flexDirection: "column", alignItems: "center" }} to='/signin' >
-                <ThemeProvider theme={theme}>
-                    <Button style={{ width: '220%', fontSize: "small" }} variant="contained" color="primary" className={classes.margin}>
-                        יש לך כבר משתמש?
-                        עבור למסך ההתחברות
-                    </Button>
-                </ThemeProvider>
-            </Link>
-        </div>
-    </section>)
-}
+          <ThemeProvider theme={theme}>
+            <Button
+              style={{ width: "300%", left: 85 }}
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.margin}
+            >
+              <span style={{ fontSize: "large" }}>הרשם עכשיו</span>
+            </Button>
+          </ThemeProvider>
+        </form>
+        <hr style={{ border: "1px solid grey", width: "98%" }} />
+        <Link
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+          to="/signin"
+        >
+          <ThemeProvider theme={theme}>
+            <Button
+              style={{ width: "220%", fontSize: "small" }}
+              variant="contained"
+              color="primary"
+              className={classes.margin}
+            >
+              יש לך כבר משתמש? עבור למסך ההתחברות
+            </Button>
+          </ThemeProvider>
+        </Link>
+      </div>
+    </section>
+  );
+};
 
 export default Signup;
-
